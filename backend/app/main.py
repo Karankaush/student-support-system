79fffda7d362dc78438dae517d806340a62800cd
karan <learnwithfacts05@gmail.com>
2026-09-24 23:35:16 +0530

    feat: enable frontend CORS

--- backend/app/main.py
+++ backend/app/main.py
@@ -3,12 +3,23 @@ from app.routers.auth import router as auth_router
 from app.db.database import Base, engine
 from app.routers.tickets import router as ticket_router
 from app.models import User, Ticket, TicketActivity
-
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(
     title="Student Support & Ticket Management System",
     version="1.0.0",
 )
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
 app.include_router(auth_router)
 app.include_router(ticket_router)
 
